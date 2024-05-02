cccf02a02a1bb72843cae1c4b1503666e8a1d655
charltonaustin <charlton.austin@gmail.com>
2024-05-02 13:59:14 -0700

    Adding in a new function for updating y

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,13 @@ pub fn empty_board() -> String {
 }
 
 pub fn add_x(mut board: String, position: &str) -> String {
+    add_symbol(&mut board, position, "x")
+}
+
+pub fn add_y(mut board: String, position: &str) -> String {
+    add_symbol(&mut board, position, "x")
+}
+fn add_symbol(board: &mut String, position: &str, symbol: &str) -> String {
     let index = match position {
         "a1" => 36,  // the index of the first '-' in "1  -  |  -  |  -"
         "b1" => 42,
@@ -23,11 +30,14 @@ pub fn add_x(mut board: String, position: &str) -> String {
         "a3" => 136, // the index of the first '-' in "3  -  |  -  |  -"
         "b3" => 142,
         "c3" => 148,
-        _ => return board,  // return the board unchanged if position is invalid
+        _ => return board.to_string(),  // return the board unchanged if position is invalid
     };
-    board.replace_range(index..index+1, "x");  // replace the character at the calculated index
+
+    board.replace_range(index..index + 1, symbol);  // replace the character at the calculated index
     board.to_string()
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -124,5 +134,82 @@ mod tests {
 3  x  |  -  |  -
       |     |     ";
         assert_eq!(board, add_x(empty_board(), "a3"));
+    }
+    #[test]
+    fn test_add_y_a1() {
+        let board = "
+    a     b     c
+      |     |
+1  y  |  -  |  -
+ _____|_____|_____
+      |     |
+2  -  |  -  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!(board, add_y(empty_board(), "a1"));
+    }
+    #[test]
+    fn test_add_y_b1() {
+        let board = "
+    a     b     c
+      |     |
+1  -  |  y  |  -
+ _____|_____|_____
+      |     |
+2  -  |  -  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!(board, add_y(empty_board(), "b1"));
+    }
+
+    #[test]
+    fn test_add_y_c1() {
+        let board = "
+    a     b     c
+      |     |
+1  -  |  -  |  y
+ _____|_____|_____
+      |     |
+2  -  |  -  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!(board, add_y(empty_board(), "c1"));
+    }
+
+    #[test]
+    fn test_add_y_a2() {
+        let board = "
+    a     b     c
+      |     |
+1  -  |  -  |  -
+ _____|_____|_____
+      |     |
+2  y  |  -  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!(board, add_y(empty_board(), "a2"));
+    }
+    #[test]
+    fn test_add_y_a3() {
+        let board = "
+    a     b     c
+      |     |
+1  -  |  -  |  -
+ _____|_____|_____
+      |     |
+2  -  |  -  |  -
+ _____|_____|_____
+      |     |
+3  y  |  -  |  -
+      |     |     ";
+        assert_eq!(board, add_y(empty_board(), "a3"));
     }
 }
