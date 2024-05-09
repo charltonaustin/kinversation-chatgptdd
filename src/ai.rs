2bd2c6cc7a05f8d574dfb3483af6b557a707f04b
charltonaustin <charlton.austin@gmail.com>
2024-05-09 13:13:36 -0700

    Adding in a failing test.

--- src/ai.rs
+++ src/ai.rs
@@ -4,9 +4,22 @@ pub fn make_move(mut board: String) -> String{
 
 #[cfg(test)]
 mod tests {
+    use crate::ai::make_move;
+
     #[test]
     fn test_make_move() {
-
+        let board = "
+    a     b     c
+      |     |
+1  -  |  -  |  x
+ _____|_____|_____
+      |     |
+2  -  |  -  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!("b2", make_move(board.to_string()));
     }
 
 }
