e679aa9f43a349fc29cb777a944d076097e1a6b1
charltonaustin <charlton.austin@kin.com>
2024-05-16 14:02:18 -0700

    Adding in updated AI to get better scores.

--- src/ai.rs
+++ src/ai.rs
@@ -2,19 +2,22 @@ mod is_full;
 mod is_winner;
 use is_winner::is_winner;
 use is_full::is_full;
-pub fn make_move(board: String) -> String {
+pub fn make_move(board: String, to_win: String, to_lose: String) -> String {
     let mut best_score = i32::MIN;
     let mut best_move = String::new();
 
+    if board.chars().nth(92).unwrap() == '-' {
+        return "b2".to_string()
+    }
     // Simulate placing 'x' in each possible position and evaluate using minimax
-    for (position, index) in positions().iter() {
+    for (_position, index) in positions().iter() {
         if board.chars().nth(*index).unwrap() == '-' {
             let mut new_board = board.clone();
-            new_board.replace_range(*index..=*index, "x");
-            let score = minimax(new_board, false);
+            new_board.replace_range(*index..=*index, &to_win);
+            let score = minimax(new_board, to_win.to_string(), to_lose.to_string(),true);
             if score > best_score {
                 best_score = score;
-                best_move = position.to_string();
+                best_move = _position.to_string();
             }
         }
     }
@@ -22,8 +25,8 @@ pub fn make_move(board: String) -> String {
     best_move
 }
 
-fn minimax(board: String, is_maximizing: bool) -> i32 {
-    if is_winner(&board, 'x') {
+fn minimax(board: String, to_win: String, to_lose: String, is_maximizing: bool) -> i32 {
+    if is_winner(&board, to_win.chars().next().unwrap()) {
         return 1;
     } else if is_winner(&board, 'o') {
         return -1;
@@ -33,22 +36,22 @@ fn minimax(board: String, is_maximizing: bool) -> i32 {
 
     if is_maximizing {
         let mut best_score = i32::MIN;
-        for (position, index) in positions().iter() {
+        for (_position, index) in positions().iter() {
             if board.chars().nth(*index).unwrap() == '-' {
                 let mut new_board = board.clone();
-                new_board.replace_range(*index..=*index, "x");
-                let score = minimax(new_board, false);
+                new_board.replace_range(*index..=*index, &to_win);
+                let score = minimax(new_board, to_win.to_string(), to_lose.to_string(), false);
                 best_score = i32::max(best_score, score);
             }
         }
         best_score
     } else {
         let mut best_score = i32::MAX;
-        for (position, index) in positions().iter() {
+        for (_position, index) in positions().iter() {
             if board.chars().nth(*index).unwrap() == '-' {
                 let mut new_board = board.clone();
-                new_board.replace_range(*index..=*index, "y");
-                let score = minimax(new_board, true);
+                new_board.replace_range(*index..=*index, &to_lose);
+                let score = minimax(new_board, to_win.to_string(), to_lose.to_string(), false);
                 best_score = i32::min(best_score, score);
             }
         }
@@ -58,9 +61,9 @@ fn minimax(board: String, is_maximizing: bool) -> i32 {
 
 fn positions() -> Vec<(&'static str, usize)> {
     vec![
-        ("a1", 31), ("b1", 37), ("c1", 43),
-        ("a2", 79), ("b2", 85), ("c2", 91),
-        ("a3", 127), ("b3", 133), ("c3", 139)
+        ("b2", 92), ("b1", 42), ("c1", 48),
+        ("a2", 86), ("a1", 36), ("c2", 98),
+        ("a3", 136), ("b3", 142), ("c3", 148)
     ]
 }
 #[cfg(test)]
@@ -68,7 +71,7 @@ mod tests {
     use crate::ai::make_move;
 
     #[test]
-    fn test_make_move() {
+    fn test_always_pick_middle_with_top_right() {
         let board = "
     a     b     c
       |     |
@@ -80,7 +83,70 @@ mod tests {
       |     |
 3  -  |  -  |  -
       |     |     ";
-        assert_eq!("b2", make_move(board.to_string()));
+        assert_eq!("b2", make_move(board.to_string(), "o".to_string(), "x".to_string()));
+    }
+
+    #[test]
+    fn test_always_pick_middle_with_top_left() {
+        let board = "
+    a     b     c
+      |     |
+1  x  |  -  |  -
+ _____|_____|_____
+      |     |
+2  -  |  -  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!("b2", make_move(board.to_string(), "o".to_string(), "x".to_string()));
+    }
+
+    #[test]
+    fn test_pick_with_empty_board() {
+        let board = "
+    a     b     c
+      |     |
+1  -  |  -  |  -
+ _____|_____|_____
+      |     |
+2  -  |  -  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!("b2", make_move(board.to_string(), "o".to_string(), "x".to_string()));
+    }
+
+    #[test]
+    fn test_pick_stop_win() {
+        let board = "
+    a     b     c
+      |     |
+1  x  |  x  |  -
+ _____|_____|_____
+      |     |
+2  -  |  o  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!("c1", make_move(board.to_string(), "o".to_string(), "x".to_string()));
+    }
+    #[test]
+    fn test_pick_best_move_for_o() {
+        let board = "
+    a     b     c
+      |     |
+1  x  |  x  |  -
+ _____|_____|_____
+      |     |
+2  o  |  o  |  -
+ _____|_____|_____
+      |     |
+3  -  |  -  |  -
+      |     |     ";
+        assert_eq!("c2", make_move(board.to_string(), "o".to_string(), "x".to_string()));
     }
 
 }
